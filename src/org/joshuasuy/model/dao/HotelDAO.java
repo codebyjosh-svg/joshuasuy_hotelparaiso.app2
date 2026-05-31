9d4b526fd36b29731adc9c53a3475444b246bd0d
joshuasuy <jsuy-2026083@kinal.edu.gt>
2026-05-31 08:26:53 -0600

    004 Agregue las firmas de metodo

--- src/org/joshuasuy/model/dao/HotelDAO.java
+++ src/org/joshuasuy/model/dao/HotelDAO.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package org.joshuasuy.model.dao;
 
-/**
- *
- * @author Usuario
- */
+import java.util.List;
+import org.joshuasuy.model.Hotel;
+
 public interface HotelDAO {
+    
+    boolean insertar(Hotel hotel);
+    List<Hotel> listar();
+    Hotel buscar(String id);
+    boolean actualizar(Hotel hotel);
+    boolean eliminar(String id);
     
 }
