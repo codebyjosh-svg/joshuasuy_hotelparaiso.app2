7e5612e0d959a0c205f95e63864fe2fcaa0bd938
joshuasuy <jsuy-2026083@kinal.edu.gt>
2026-05-31 08:13:43 -0600

    003 cree la conexion con mi usuario de MySQL

--- src/org/joshuasuy/model/conexion/Conexion.java
+++ src/org/joshuasuy/model/conexion/Conexion.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.joshuasuy.model.conexion;
 
-/**
- *
- * @author Usuario
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
     
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/hotelparaisodb_in4cm?serverTimezon=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "suy.24";
+    
+    public static Connection conectar(){
+        connection conexion = null;
+        
+        try{
+            class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+        }catch (ClassNotFounException e){
+            System.err.print("ERROR:nohay driver.");
+            
+        }catch (SQLException ex){
+            System.err.print("ERROR: no se pudo conectar a la DB"+ ex.getMessage());
+        }
+        
+        return conexion;
+    }
 }
