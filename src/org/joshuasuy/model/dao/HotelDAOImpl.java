35bf2425f377392a370207fb167bda0372908171
joshuasuy <jsuy-2026083@kinal.edu.gt>
2026-05-31 08:42:29 -0600

    005 DAO completo Interfaz y Implementacion

--- src/org/joshuasuy/model/dao/HotelDAOImpl.java
+++ src/org/joshuasuy/model/dao/HotelDAOImpl.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.joshuasuy.model.dao;
 
-/**
- *
- * @author Usuario
- */
-public class HotelDAOImpl {
+import java.util.ArrayList;
+import java.util.List;
+import org.joshuasuy.model.Hotel;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HotelDAOImpl implements HotelDAO {
     
+    @Override
+    public boolean insertar(Hotel hotel){
+        return false;
+    }
+    
+    @Override
+    @SuppressWarnings("empty-statement")
+    public List<Hotel>listar(){
+        List<Hotel> hoteles = new ArrayList<>();
+        String consulta ="{call sp_listarhotel()}";
+        try (Connection conexion = conexion.conectar();
+            CallableStatemen call = conexion.prepareCall(consulta);
+            ResultSet resultado = call executeQuery()){
+         while (resultado.next()){
+             hoteles.add(new Hotel(
+                     resultado.getString("ocupaciones"),
+                     resultado.getString("tipo_de_habitacion"),
+                     resultado.getString("servicios")
+             ));
+         }
+        } catch (SQLException e){
+                System.err.println("ERROR: al listar hotel: "+ e.get Message());
+        }
+        return hoteles;
+        
+    }
+    
+    @Override
+    public Hotel buscar(String id){
+        return null;
+    }
+    
+    @Override
+    public boolean actualizar(Hotel hotel){
+        return false;
+    }
+    
+    @Override
+    public boolean eliminar (String id){
+        return false;
+    }
 }
