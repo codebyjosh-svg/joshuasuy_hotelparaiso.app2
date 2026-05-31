cf36c43f8a6372f0d7c89136840a6897cdfa2b06
joshuasuy <jsuy-2026083@kinal.edu.gt>
2026-05-30 18:19:49 -0600

    002 Modifique la clase Hotel

--- src/org/joshuasuy/model/Hotel.java
+++ src/org/joshuasuy/model/Hotel.java
@@ -1,14 +1,43 @@
 package org.joshuasuy.model;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario
- */
 public class Hotel {
+   
+    private String ocupaciones;
+    private String tipoDeHabitacion;
+    private String servicios;
     
+// Construimos los VACÍOS 
+public Hotel(){
+}
+
+public Hotel(String ocupaciones, String tipoDeHabitacion, String servicios){
+    this.ocupaciones = ocupaciones;
+    this.tipoDeHabitacion = tipoDeHabitacion;
+    this.servicios = servicios;
+}
+
+// creamos los Getters y Setters
+public String getOcupaciones(){
+    return ocupaciones;
+}
+
+public void setOcupaciones(String ocupaciones){
+    this.ocupaciones = ocupaciones;
+}
+
+public String getTipoDeHabitacion(){
+    return tipoDeHabitacion;
+}
+
+public void setTipoDeHabitacion(String tipoDeHabitacion){
+    this.tipoDeHabitacion = tipoDeHabitacion;
+}
+
+public String getServicios(){
+    return servicios;
+}
+
+public void setServicios(String servicios){
+    this.servicios = servicios;
+}
 }
